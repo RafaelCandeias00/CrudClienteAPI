87922de657c19699c89af56ec3c9e55107ef051a
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-03-05 15:33:40 -0300

    @RafaelCandeias00 [Task: add ValidaCPF e inserir formato de Data]

---
+++ ClienteAPI/ClienteAPI/Migrations/20230305181643_AlterandoAtributoDocumento.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClienteAPI.Migrations
+{
+    public partial class AlterandoAtributoDocumento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Clientes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "Clientes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Clientes",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- ClienteAPI/ClienteAPI/Models/Cliente.cs
+++ ClienteAPI/ClienteAPI/Models/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClienteAPI.Validations;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ClienteAPI.Models
@@ -8,18 +9,20 @@ namespace ClienteAPI.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
-        public string Documento { get; set; }
+        [Required(ErrorMessage = "CPF é obrigatório!")]
+        [ValidaCpf(ErrorMessage = "CPF Inválido!")]
+        public string Cpf { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataNascimento { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "")]
         [StringLength(9, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 8)]
         public string Sexo { get; set; }
 
